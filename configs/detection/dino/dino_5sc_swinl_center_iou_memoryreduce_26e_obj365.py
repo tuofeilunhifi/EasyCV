f8b48d1659fc782814da7879e50558e973d24087
tuofeilunhifi <18292877852@163.com>
2022-10-18 14:16:37 +0800

    bugfix

--- configs/detection/dino/dino_5sc_swinl_center_iou_memoryreduce_26e_obj365.py
+++ configs/detection/dino/dino_5sc_swinl_center_iou_memoryreduce_26e_obj365.py
@@ -1,5 +1,6 @@
 _base_ = [
-    './dino_5sc_swinl.py', '../common/dataset/autoaug_obj365_detection.py',
+    './dino_5sc_swinl.py',
+    '../common/dataset/autoaug_obj365_val5k_detection.py',
     './dino_schedule_1x.py'
 ]
 
