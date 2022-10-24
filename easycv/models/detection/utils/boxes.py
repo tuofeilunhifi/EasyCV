cbac3772860f8950a7a2dda85e4d63710eff2ffc
tuofeilunhifi <18292877852@163.com>
2022-10-24 17:45:50 +0800

    bugfix

--- easycv/models/detection/utils/boxes.py
+++ easycv/models/detection/utils/boxes.py
@@ -90,10 +90,10 @@ def generalized_box_iou(boxes1, boxes2):
     # degenerate boxes gives inf / nan results
     # so do an early check
     # assert (boxes1[:, 2:] >= boxes1[:, :2]).all()
+    keep = boxes1[:, 2:] >= boxes1[:, :2]
+    boxes1[:, 2:] = torch.where(keep, boxes1[:, 2:], boxes1[:, :2])
     assert (boxes2[:, 2:] >= boxes2[:, :2]).all()
     iou, union = box_iou(boxes1, boxes2)
-    if not (boxes1[:, 2:] >= boxes1[:, :2]).all():
-        return torch.zeros_like(iou)
 
     lt = torch.min(boxes1[:, None, :2], boxes2[:, :2])
     rb = torch.max(boxes1[:, None, 2:], boxes2[:, 2:])
